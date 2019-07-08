e4020154282aac7cc90c06a57a18c2672b84627e
ANRAS <pierre.anras@cgi.com>
2019-07-08 12:30:32 +0200

    Ajout du bouton fonctionel sur la page d'accueil et lien vers la page Liste Client au clic

--- app/src/main/java/com/example/hub2switch/activite_accueil.java
+++ app/src/main/java/com/example/hub2switch/activite_accueil.java
@@ -1,18 +1,32 @@
 package com.example.hub2switch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activite_accueil extends AppCompatActivity {
 
 
-
+private Button bt_accueil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_accueil);
+
+        bt_accueil = (Button) findViewById(R.id.bouton_accueil);
+
+        bt_accueil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goTo_liste_client = new Intent(activite_accueil.this, liste_client.class);
+                startActivity(goTo_liste_client);
+            }
+
+        });
 
     }
 }
