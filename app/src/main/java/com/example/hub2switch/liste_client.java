76c9bb01396d71b673f3e8d136afa2562aee0b82
CSBuzzy <srebuzzi@gmail.com>
2019-07-09 16:11:40 +0200

    création interface liste_client + actionListener des boutons

--- app/src/main/java/com/example/hub2switch/liste_client.java
+++ app/src/main/java/com/example/hub2switch/liste_client.java
@@ -1,16 +1,40 @@
 package com.example.hub2switch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class liste_client extends AppCompatActivity {
 
-
+    private ImageButton btnClientAccess;
+    private ImageButton btnNewClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_liste_client);
+
+        btnClientAccess = (ImageButton) findViewById(R.id.bouton_client_access);
+        btnClientAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goTo_fiche_client = new Intent(liste_client.this, fiche_client.class);
+                startActivity(goTo_fiche_client);
+            }
+
+        });
+
+        btnNewClient = (ImageButton) findViewById(R.id.bouton_new_client);
+        btnNewClient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent goTo_new_client = new Intent(liste_client.this, ajout_client.class);
+                startActivity(goTo_new_client);
+            }
+        });
 
     }
 }
